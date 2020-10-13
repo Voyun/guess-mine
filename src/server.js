e5808c2fd1635933a280f097b3bae382addd655b
Voyun <voyun0819@gmail.com>
2020-10-13 21:05:36 +0900

    3.socket event: send & join

--- src/server.js
+++ src/server.js
@@ -1,16 +1,28 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(express.static(join(__dirname, "static")));
+app.use(logger("dev"));
 app.get("/", (req,res) => res.render("home"));
 
-const handleListening = () => console.log(`✅ Server running: https://localhost;${PORT}`);
+const handleListening = () => console.log(`✅ Server running: https://localhost:${PORT}`);
 
 const server =  app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+io.on("connection", (socket) => {
+    socket.on("HelloGuys", () => console.log("the client said hello"));
+});
+
+
+
+//setInterval(() => console.log(sockets), 1000);
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,6 @@
-hello;
+// eslint-disable-next-line no-undef
+const socket = io("/");
+
+socket.on("Hello", () => console.log("Somebody joined!"));
+
+setTimeout( () => socket.emit("HelloGuys"), 5000);
